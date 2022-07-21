d2947acb04d23c9093dee2ff6eab1357ffaca393
GACHIB0SS <der.confederate@gmail.com>
2022-07-21 22:05:34 +0500

    pew

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
+
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USER = "root";
+    private static final String PASS  = "root";
+
+    public static Connection connection;
+    static {
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASS);
+            System.out.println("Connection established");
+        } catch (SQLException ex) {
+            System.out.println("Connection not established");
+            ex.printStackTrace();
+        }
+    }
 }
